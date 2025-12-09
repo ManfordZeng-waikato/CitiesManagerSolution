163e0806f02f261a5c1248494efe063609e7d7a4
ManfordZeng-waikato <zengchang389@gmail.com>
2025-12-09 14:51:37 +1300

    Create JwtService for generating Jwt token

---
+++ CitiesManager.Core/Services/JwtService.cs
@@ -0,0 +1,57 @@
+﻿using CitiesManager.Core.DTO;
+using CitiesManager.Core.Identity;
+using CitiesManager.Core.ServiceContracts;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace CitiesManager.Core.Services
+{
+    public class JwtService : IJwtService
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public AuthenticationResponse CreateJwtToken(ApplicationUser user)
+        {
+            DateTime expiration =
+            DateTime.UtcNow.AddMinutes(Convert.ToDouble(_configuration["Jwt:EXPIRATION_MINUTES"]));
+
+            Claim[] claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Iat,DateTime.UtcNow.ToString()),
+                new Claim(ClaimTypes.Name,user.PersonName)
+            };
+
+            SymmetricSecurityKey securitykey = new(
+                Encoding.UTF8.GetBytes(_configuration["Jwt:KEY"]));
+
+            SigningCredentials signinCredentials = new(securitykey, SecurityAlgorithms.HmacSha256);
+
+            JwtSecurityToken tokenGenerator = new(
+                issuer: _configuration["Jwt:ISSUER"],
+                audience: _configuration["Jwt:AUDIENCE"],
+                claims: claims,
+                expires: expiration,
+                signingCredentials: signinCredentials
+                );
+
+            string tokenAsString = new JwtSecurityTokenHandler().WriteToken(tokenGenerator);
+
+            return new AuthenticationResponse
+            {
+                PersonName = user.PersonName!,
+                Email = user.Email!,
+                Token = tokenAsString,
+                Expiration = expiration
+            };
+        }
+    }
+}
